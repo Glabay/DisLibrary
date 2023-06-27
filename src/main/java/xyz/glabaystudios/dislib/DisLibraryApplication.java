e31ab122ba078f95dab77d4e104ad4bfd89741da
Glabay <java_guru@live.com>
2023-06-27 11:01:32 -0300

    June 27th, 2023 - Initial Commit
    Signed-off-by: Glabay <Java_Guru@live.com>

---
+++ src/main/java/xyz/glabaystudios/dislib/DisLibraryApplication.java
@@ -0,0 +1,13 @@
+package xyz.glabaystudios.dislib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DisLibraryApplication.class, args);
+    }
+
+}
