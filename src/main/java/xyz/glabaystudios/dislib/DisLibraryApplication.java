79a0594b488c77963ac0c3a1fb94f9807b535b6c
Glabay <java_guru@live.com>
2023-06-28 02:34:15 -0300

    June 27th, 2023 - Second video
    Notes:
    - Created a configuration for the project
    
    Signed-off-by: Glabay <Java_Guru@live.com>

--- src/main/java/xyz/glabaystudios/dislib/DisLibraryApplication.java
+++ src/main/java/xyz/glabaystudios/dislib/DisLibraryApplication.java
@@ -2,8 +2,10 @@ package xyz.glabaystudios.dislib;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("xyz.glabaystudios")
 public class DisLibraryApplication {
 
     public static void main(String[] args) {
